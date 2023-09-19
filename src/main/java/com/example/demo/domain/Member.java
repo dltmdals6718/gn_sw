913d50e219afdaf5938b05c1e231cd4b220d5896
ysbin0505 <chfhddl515@gmail.com>
2023-09-19 14:00:14 +0900

    ??

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -3,12 +3,9 @@ package com.example.demo.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Entity
-@Getter @Setter
 public class Member {
 
     @Id
