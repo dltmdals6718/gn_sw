ecb9fdef4173510b5472bbf573b7fc1c277bcb90
LSM <dltmdals6718@naver.com>
2023-10-04 20:15:57 +0900

    main과 맞춤

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -1,8 +1,6 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
@@ -10,12 +8,10 @@ import lombok.Setter;
 
 @Data
 @Entity
-@Getter
-@Setter
+@Getter @Setter
 public class Member {
 
-    @Id @GeneratedValue
-    @Column(name = "MEMBER_ID")
+    @Id
     private Long id;
     private String nickname;
     private String email;
